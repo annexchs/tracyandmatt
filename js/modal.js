0fb64b7de8a9a8dedb7bace98432767af178c08c
Bradley Woods <brad@Bradleys-MacBook-Pro.local>
2015-07-30 21:04:01 -0400

    finished modal visibility logic. email still needs to be done

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+    checkEmail();
     toggleFields(); //call this first so we start out with the correct visibility depending on the selected form values
    //this will call our toggleFields function every time the selection value of our underAge field changes
     $("#plus1").change(function() { toggleFields(); });
+    
+    $("#email").change(function() { checkEmail(); });
 
     $("#submit-button").submit(function() {
       
@@ -20,4 +23,17 @@ function toggleFields()
       $("#plus1Middle").hide();
       $("#plus1Last").hide();
     }
+}
+
+function checkEmail()
+{
+  var emails = ["test@email.com"];
+  
+  console.log(jQuery.inArray($("#email").val(), emails) !== -1);
+  if (jQuery.inArray($("#email").val(), emails) !== -1) {
+    $("#plus1-column").show();
+  }
+  else {
+    $("#plus1-column").hide();
+  }
 }
